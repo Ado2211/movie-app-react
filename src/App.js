7aea16e96398d419fb4e42ca83a52efc3bc0d1fe
Adnan Mahmić <mahmica@gmail.com>
2022-05-24 03:31:05 +0200

    cars api

--- src/App.js
+++ src/App.js
@@ -6,59 +6,50 @@ import MovieCard from './MovieCard';
 import './App.css';
 import SearchIcon from './search.svg';
 
-const API_URL = 'http://www.omdbapi.com?apikey=6f3e785d';
+
+
+import axios from "axios";
+
+
+const API_URL = 'car-data.p.rapidapi.com?apikey=7786f90c70msh921a76a3839a3e0p1a7ce7jsnc8b7cda04a74';
 
 
 const App = () => {
-    const [movies, setMovies] = useState([]);
-    const [searchTerm, setSearchTerm] = useState('');
+    const options = {
+        method: 'GET',
+        url: 'https://car-data.p.rapidapi.com/cars/makes',
+        headers: {
+            'X-RapidAPI-Host': 'car-data.p.rapidapi.com',
+            'X-RapidAPI-Key': '7786f90c70msh921a76a3839a3e0p1a7ce7jsnc8b7cda04a74'
+        }
+    };
 
-    const searchMovies = async (title) => {
-        const response = await fetch(`${API_URL}&s=${title}`);
-        const data = await response.json();
+
+    axios.request(options).then(function (response) {
+        const makes = (response.data);
+
+       
+    }).catch(function (error) {
+        console.error(error);
+
+     
+    });
     
-        setMovies(data.Search);
-    }
-
-    useEffect(() => {
-        searchMovies('Spiderman')
-    }, []);
-
     return (
-        <div className="app">
-            <h1>MovieLand</h1>
-
-            <div className="search">
-                <input
-                    placeholder="Search for movies"
-                    value={searchTerm}
-                    onChange={(e) => setSearchTerm(e.target.value)}
-                />
-                <img
-                    src={SearchIcon}
-                    alt="search"
-                    onClick={() => searchMovies(searchTerm)}
-                />
-            </div>
-
-            {
-                movies?.length > 0
-                    ? (
-                        <div className='container'>
-                            {movies.map((movie) => (
-                                <MovieCard movie={movie}/>
-                           ))}
-
-                        </div>
-                    ) : (
-                        <div className='empty'>
-                            <h2>No movies found</h2>
-                        </div>
-                    )
-            }
-
-
+        axios.request(options).then(function (response) {
+            const makes = (response.data);
+    
+           
+        }).catch(function (error) {
+            console.error(error);
+    
+         
+        }),
+        <div className="container">
+            <p>{console.log(response.data)}</p>
         </div >
+
+
     );
 }
 
